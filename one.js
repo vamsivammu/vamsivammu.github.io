c3ccf7343e40d136f5430f776b1a6db7a2a0c73c
vamsivammu <38162892+vamsivammu@users.noreply.github.com>
2018-04-07 22:10:29 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,17 @@
+function add() {
+	document.getElementById('ans').innerHTML="Answer is " + Number(document.getElementById('one').value)+Number(document.getElementById('two').value);	
+}
+function divide() {
+
+	document.getElementById('ans').innerHTML="Answer is " +  Number(document.getElementById('one').value)/Number(document.getElementById('two').value);	
+}
+function multiply() {
+	document.getElementById('ans').innerHTML="Answer is " + Number(document.getElementById('one').value)*Number(document.getElementById('two').value);	
+}
+function subtract() {
+	document.getElementById('ans').innerHTML= Number(document.getElementById('one').value)-Number(document.getElementById('two').value);	
+}
+
+
+
+
