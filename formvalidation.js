8ea87797b9dca319861d69e4185e1564e719b46a
vamsivammu <38162892+vamsivammu@users.noreply.github.com>
2018-05-15 17:03:21 +0530

    Update formvalidation.js

--- formvalidation.js
+++ formvalidation.js
@@ -1,1 +1,11 @@
 var x=angular.module("formvalidate",[])
+x.controller('formcontroller', function($scope){
+
+
+$scope.mobilenumber=/^[0-9]{10,12}$/; 
+$scope.fullname=/^[a-zA-Z]{1,}$/;
+$scope.electronicmail=
+
+
+
+})
